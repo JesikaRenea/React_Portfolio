5584fa2c29e546e3aa2c78cd6e231899a786f825
JesikaRenea <JessicaHayesUCF@outlook.com>
2020-05-29 16:48:16 -0400

    Updates

---
+++ precache-manifest.cc7c202b8fcb1cb0814d07117d0b6125.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1acd9b6de8ea759eb77fb579645fc9bd",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "1cdf0dc17bf0d3924f66",
+    "url": "/React_Portfolio/static/css/main.1f451966.chunk.css"
+  },
+  {
+    "revision": "3b4d821123051119780b",
+    "url": "/React_Portfolio/static/js/2.0f514376.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/React_Portfolio/static/js/2.0f514376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cdf0dc17bf0d3924f66",
+    "url": "/React_Portfolio/static/js/main.7bae371d.chunk.js"
+  },
+  {
+    "revision": "8248011d6a5babd44a4f",
+    "url": "/React_Portfolio/static/js/runtime-main.f89238f9.js"
+  },
+  {
+    "revision": "affcaef52a1aa1a8b48f862c9d420265",
+    "url": "/React_Portfolio/static/media/LocalWire_Project7.affcaef5.png"
+  },
+  {
+    "revision": "5f3a021b60ba1255367e84f3d2d2ea4b",
+    "url": "/React_Portfolio/static/media/ucfLogo.5f3a021b.jpg"
+  }
+]);
