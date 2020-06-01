42f9a9a37882eedd376bcbbd0c900cf1f202517c
JesikaRenea <JessicaHayesUCF@outlook.com>
2020-06-01 16:49:47 -0400

    Test Deployment 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,27 +3,27 @@ import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
-    return (
-        <div>
-  <nav>
-    <div class="nav-wrapper">
-      <a href="/" class="brand-logo right">JH</a>
-      <ul id="nav-mobile" class="left hide-on-med-and-down">
+  return (
+    <div>
+      <nav>
+        <div class="nav-wrapper">
+          <Link to="/" class="brand-logo right">JH</Link>
+          <ul id="nav-mobile" class="left hide-on-med-and-down">
 
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/fs-portfolio">Full-Stack Portfolio</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/fs-portfolio">Full-Stack Portfolio</Link></li>
+{/* 
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/fs-portfolio">Full-Stack Portfolio</a></li> */}
 
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/fs-portfolio">Full-Stack Portfolio</a></li>
-
-      </ul>
-    </div>
-  </nav>
-        
+          </ul>
         </div>
-    );
+      </nav>
+
+    </div>
+  );
 };
 
 export default Navbar
