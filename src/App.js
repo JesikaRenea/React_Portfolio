d786928052abe5d32e984a1fb3d1dcb1849a67b7
JesikaRenea <JessicaHayesUCF@outlook.com>
2020-06-03 17:24:20 -0400

    Updated Portfolio.json data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import 'materialize-css';
-// import { Button, Card, Row, Col } from 'react-materialize';
 import About from './components/About';
 import Contact from './components/Contact';
 import FsPortfolio from './components/FsPortfolio';
@@ -14,20 +13,21 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-              <div className="container">
+      <div className="container">
 
-      <Router>
-        <Header />
-        <Navbar />
-        <main>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/fs-portfolio" component={FsPortfolio} />
-        </main>
-        <Footer />
-      </Router>
-</div>
+        <Router>
+          <Header />
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/fs-portfolio" component={FsPortfolio} />
+          </ Switch>
+          <Redirect to="/about" />
+          <Footer />
+        </Router>
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import LocalWire from "../assets/images/LocalWire_Project7.png";
 
@@ -7,11 +9,13 @@ const Card = props => {
 
             <div class="card">
                 <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src={props.image} />
+                    <img class="activator"
+                        src={props.image} />
                 </div>
                 <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">{props.title}<i class="material-icons right">...</i></span>
-                    <p><a href={props.href} target="_blank">View Project</a></p>
+                    <p><a href={props.href}
+                        target="_blank">View Project</a></p>
                 </div>
                 <div class="card-reveal">
                     <span class="card-title grey-text text-darken-4">{props.title}<i
